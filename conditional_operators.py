a6f68a47e7eaf9184a2627a2ffebf418d78a69c3
Narek <narek.grigoryan1993@gmail.com>
2025-10-07 17:25:29 +0400

    Learn ternary operator (one-line if/else)

--- conditional_operators.py
+++ conditional_operators.py
@@ -17,9 +17,11 @@
 
 data = input()
 
-if data == "Five":
-    number = 5 
-else:
-    number = 0 
+number = 5 if data == "Five" else 0 
+
+# if data == "Five":
+#     number = 5 
+# else:
+#     number = 0 
 
 print (number)        
