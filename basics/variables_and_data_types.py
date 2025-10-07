41b394c9bc4545997999d9ae29fe9c99b00cc9fa
Narek <narek.grigoryan1993@gmail.com>
2025-10-07 20:07:16 +0400

    Move lesson files to basics folder

---
+++ basics/variables_and_data_types.py
@@ -0,0 +1,39 @@
+# number = 5 # int
+
+# digit = 4.23423453245 # float
+# word = "result" # string
+
+# print("word", digit)
+
+# del number 
+
+# number = 7
+# print("result:", number)
+
+
+# number = 5 # int
+
+# digit = 4.23423453245 # float
+# word = "result:"
+# boolean = True # bool
+
+# str_num = "5" # string 
+
+# # print("word" + str(digit))
+# # print(word, number + int(str_num)) 
+# print(word + str(number + int(str_num)))
+
+# del number 
+
+
+
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("Result:", num1 + num2)
+print("Result:", num1 - num2)
+print("Result:", num1 / num2)
+print("Result:", num1 * num2)
+print("Result:", num1 ** num2)
+print("Result:", num1 // num2)
