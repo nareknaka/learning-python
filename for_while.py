290a9297cd3a502ce2b1b4d05abec20652162e4c
Narek <narek.grigoryan1993@gmail.com>
2025-10-07 19:56:37 +0400

    Add for-else loop with search algorithm

--- for_while.py
+++ for_while.py
@@ -16,11 +16,23 @@
         
 
 
-for i in range(1, 11):
-    if i == 5:
+# for i in range(1, 11):
+#     if i == 5:
+#         break
+    
+#     if i % 2 == 0:
+#         continue
+    
+#     print(i)
+
+
+
+found = None 
+for i in "hello":
+    if i == "l":
+        found = True
         break
-    
-    if i % 2 == 0:
-        continue
-    
-    print(i)
+else:
+    found = False
+
+print(found)    
