5b69fe3c86ee80d34cd61137aa9c0fc612bc30e2
Narek <narek.grigoryan1993@gmail.com>
2025-10-07 19:44:52 +0400

    Learn while loop and loop control

--- for_while.py
+++ for_while.py
@@ -1,11 +1,16 @@
 # for i in range(1, 6):
 #     print(i)
 
-count = 0 
-word = "Hello World!"
-for i in word:
-    if i == "l":
-        count += 1
-print ("Count:", count)
+# count = 0 
+# word = "Hello World!"
+# for i in word:
+#     if i == "l":
+#         count += 1
+# print ("Count:", count)
 
+
+i = 5 
+while i <= 15:
+    print(i)
+    i += 2
         
