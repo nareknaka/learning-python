edcafd90688fa8585cdd37892fc7a038c2073b2c
Narek <narek.grigoryan1993@gmail.com>
2025-10-07 19:35:20 +0400

    Add character counting with increment operator

--- for_while.py
+++ for_while.py
@@ -1,9 +1,11 @@
 # for i in range(1, 6):
 #     print(i)
 
-
+count = 0 
 word = "Hello World!"
 for i in word:
-    if i == "!":
-        print("yes")
+    if i == "l":
+        count += 1
+print ("Count:", count)
+
         
