dda789464067149753364ba705927ba0b47f32c7
Narek <narek.grigoryan1993@gmail.com>
2025-10-08 14:35:49 +0400

    add user input list example

--- basics/list.py
+++ basics/list.py
@@ -8,6 +8,9 @@
 
 
 
+
+
+
 # numbers = [5, 2, 7]
 
 # numbers.append(100)
@@ -27,8 +30,28 @@
 # print(len(numbers))
 
 
-nums = [5, 2, 7, "50", False]
 
-for el in nums:
-    el *= 2
-    print(el)
+
+
+
+
+# nums = [5, 2, 7, "50", False]
+
+# for el in nums:
+#     el *= 2
+#     print(el)
+
+
+
+n = int(input("Enter length: "))
+
+user_list = []
+
+i = 0
+while i < n:
+    string = "Enter element #" + str(i + 1) + ": "
+    user_list.append(input(string))
+    i += 1
+
+print(user_list)
+
