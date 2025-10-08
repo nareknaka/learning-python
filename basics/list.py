4943e2d264dfed810c0390f3c027938be4090606
Narek <narek.grigoryan1993@gmail.com>
2025-10-08 14:10:13 +0400

    Add list methods examples

--- basics/list.py
+++ basics/list.py
@@ -1,7 +1,27 @@
-nums = [5, 7, 3, 2, 4, 7, True, "Hello", 6.7]
+# nums = [5, 7, 3, 2, 4, 7, True, "Hello", 6.7  ]
 
-nums[0] = 50
-nums[5] = 1.01
+# nums[0] = 50
+# nums[5] = 1.01
 
 
-print(nums[3])
+# print(nums[7])
+
+
+
+numbers = [5, 2, 7]
+
+numbers.append(100)
+numbers.insert(1, True)
+
+b = [5, 6, 8]
+numbers.extend(b)
+# numbers.reverse()
+numbers.sort
+
+numbers.pop(1)
+numbers.remove(6)
+
+# numbers.clear() 
+
+# print(numbers.count(True))
+print(len(numbers))
