25183ee0ae9c9bd61bc20f1dadbfac5566fde501
Narek <narek.grigoryan1993@gmail.com>
2025-10-12 19:52:33 +0400

    add minimal function

--- basics/def_lambda.py
+++ basics/def_lambda.py
@@ -22,17 +22,18 @@
 
 
 
-
+def minimal(l):
+    min_number = l[0]
+    for el in l:
+        if el < min_number:
+            min_number = el
+    
+    print(min_number)
 
 
 nums1 = [5, 7, 2, 9, 4]
 
-min = nums1[0]
-for el in nums1:
-    if el < min:
-        min = el
-
-print(min)
+minimal(nums1)
 
 
 
@@ -41,9 +42,4 @@ print(min)
 
 nums2 = [5.4, 7.2, 2.3, 2.1, 9.4, 4.2]
 
-min2 = nums2[0]
-for el in nums2:
-    if el < min2:
-        min2 = el
-
-print(min2)
+minimal(nums2)
