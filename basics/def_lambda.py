4e6f71cddca9b368b118abde2d9d26a0869cd67c
Narek <narek.grigoryan1993@gmail.com>
2025-10-12 19:41:24 +0400

    add algorithm to find minimum in list

--- basics/def_lambda.py
+++ basics/def_lambda.py
@@ -10,9 +10,26 @@
 
 
 
-def summa(a, b):
-    return a + b
+# def summa(a, b):
+#     return a + b
 
-res = summa(5.5, 7.5)
-print(res)
-print(summa("H", "i"))
+# res = summa(5.5, 7.5)
+# print(res)
+# print(summa("H", "i"))
+
+
+
+
+
+
+
+
+
+nums1 = [5, 7, 2, 9, 4]
+
+min = nums1[0]
+for el in nums1:
+    if el < min:
+        min = el
+
+print(min)
