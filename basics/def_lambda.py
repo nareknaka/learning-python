7ae2431bc601f914530b9c5b85806b9d4bf4ce3c
Narek <narek.grigoryan1993@gmail.com>
2025-10-12 19:36:30 +0400

    add function to sum numbers and strings

--- basics/def_lambda.py
+++ basics/def_lambda.py
@@ -1,9 +1,18 @@
-def test_func(word):
-    print("word" , end="")
-    print("!")
+# def test_func(word):
+#     print("word" , end="")
+#     print("!")
 
 
-test_func("hi")
-test_func("3")
-test_func("5.6")
+# test_func("hi")
+# test_func("3")
+# test_func("5.6")
 
+
+
+
+def summa(a, b):
+    return a + b
+
+res = summa(5.5, 7.5)
+print(res)
+print(summa("H", "i"))
