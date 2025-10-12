1057eb1338aca356cf5185c74930ba8f614314ae
Narek <narek.grigoryan1993@gmail.com>
2025-10-12 19:27:20 +0400

    add parametr to test function

--- basics/def_lambda.py
+++ basics/def_lambda.py
@@ -1,8 +1,9 @@
-def test_func():
-    print("hello" , end="")
+def test_func(word):
+    print("word" , end="")
     print("!")
 
 
-test_func()
-test_func()
+test_func("hi")
+test_func("3")
+test_func("5.6")
 
